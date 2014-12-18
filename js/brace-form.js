2bedbda3b8d2617ad1b6fa4d226ee8a3cd556eb3
alex klibisz <aklibisz@utk.edu>
2014-12-18 03:50:53 -0500

    added why attend section, starting working on email form jquery ajax post

---
+++ js/brace-form.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+    /*$("#email-list-signup").submit(function () {
+        
+        //Get the form values
+        var subject = $('#email-list-signup [name="_subject"]').val();
+        var replyto = $('#email-list-signup [name="_replyto"]').val();
+        var gotcha = $('#email-list-signup [name="_gotcha"]').val();
+        
+        alert(subject + replyto + gotcha);
+        
+        //Submit the form values via ajax
+        $.ajax({
+            url: "//forms.brace.io/womengineersday@gmail.com",
+            method: "POST",
+            data: {'_subject' : subject, '_replyto' : replyto, '_gotcha' : gotcha},
+            dataType: "json",
+            success: function() {
+                alert("post success");
+                //Hide the form and show the success message
+                $('#email-list-signup').hide();
+                $('#email-list-signup-success').fadeIn();
+            },
+            error: function(xhr, status, error) {
+                alert(xhr.responseText);
+                //Show the error message but don't hide the form
+                $('#email-list-signup-error').fadeIn();
+            }
+        });
+        
+    });*/
+
+});
